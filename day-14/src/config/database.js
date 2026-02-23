571d8badb37d10455e471d3899caa06bc11c0212
saadkhan39 <saadsajid928@gmail.com>
2026-02-24 03:46:51 +0530

    image kit

--- day-14/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-async function connectToDb() {
-    mongoose.connect(process.env.MONGO_URI)
-    console.log("connect to DB");
-    
-}
-
-module.exports = connectToDb
