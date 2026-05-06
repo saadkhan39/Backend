c1d13afb217a603b092d33a225787ea00354179f
saadkhan39 <saadsajid928@gmail.com>
2026-05-06 23:16:41 +0530

    Socket.io

---
+++ day-23/server.js
@@ -0,0 +1,28 @@
+import app from "./src/app.js"
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+
+io.on("connection", (socket) => {
+  console.log("new collection created");
+
+
+  socket.on("message",(msg)=>{
+    console.log("user fired message event");
+    console.log(msg);
+    io.emit("abc")
+    
+  })
+  
+});
+
+
+
+httpServer.listen(3000,()=>{
+    console.log("server is running on port 3000");
+    
+});
