e782a83ab2df8d0c6baea1dc999b09ac44e8c9b7
saadkhan39 <saadsajid928@gmail.com>
2026-06-20 12:09:03 +0530

    gen ai tools

--- day-24/index.js
+++ day-24/index.js
@@ -1,7 +1,23 @@
 import "dotenv/config"
 import readline from "readline/promises";
 import { ChatMistralAI } from "@langchain/mistralai";
-import { HumanMessage } from "langchain";
+import { HumanMessage ,tool} from "langchain";
+import { createAgent } from "langchain";
+import { sendEmail } from "./mail.service.js";
+import * as z from "zod"
+
+const emailTool= tool(
+    sendEmail,
+    {
+       name:"emailTool",
+       description:"Use this tool to send an email",
+       schema:z.object({
+        to:z.string().describe("The recipient's email address"),
+        html:z.string().describe("The HTML content of the email"),
+        subject: z.string().describe("The subject of the email"),
+       })
+    }
+)
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -9,8 +25,16 @@ const rl = readline.createInterface({
 });
 
 const model = new ChatMistralAI({
-    model: "mistral-small-latest",
+    model: "mistral-small-latest"
 })
+
+const agent = createAgent({
+    model,
+    tools:[emailTool]
+})
+
+
+
 
 const messages = []
 
@@ -19,13 +43,12 @@ while (true) {
 
     messages.push(new HumanMessage(userInput))
     
-    const response = await model.invoke(messages)
+    const response = await agent.invoke({messages})
 
-    messages.push(response)
+    messages.push(response.messages[ response.messages.length - 1 ])
 
-    console.log(`\x1b[34m[AI]\x1b[0m ${response.content}`)
+    console.log(`\x1b[34m[AI]\x1b[0m ${response.messages[ response.messages.length - 1 ].content}`)
 }
 
 
 
-rl.close()
